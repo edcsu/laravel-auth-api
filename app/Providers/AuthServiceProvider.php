0ca626928bb93f5bc50c31cc9fa6098d7b44d0bc
Keith Ssewanonda <keith@laboremus.no>
2021-01-06 17:48:49 +0300

    feat:add authcontroller

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -27,5 +28,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        Passport::tokensExpireIn(Carbon::now()->addHours(1));
+
+        Passport::refreshTokensExpireIn(Carbon::now()->addHours(3));
     }
 }
